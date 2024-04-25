28ec175bdc98c1048d93e385b95d77efe8e9d51c
Sushma Ramanujam <rsushmagautam@gmail.com>
2024-04-25 19:54:16 -0400

    saving first javascripts commits to git

---
+++ run.js
@@ -0,0 +1,15 @@
+const { json } = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.post('/getName', function(req, res) {
+    res.status(400);
+    res.json({ name: 'Shrieya'});
+})
+
+console.log('Listening to port 3000')
+app.listen(3000)
